bbcfcc4e887891c8f7a9c20c5193048646ab9923
刘泽 <835052259@qq.com>
2018-09-13 12:04:28 +0800

    splash  全屏幕

--- app/src/main/java/com/lz/framecase/activity/SplashActivity.kt
+++ app/src/main/java/com/lz/framecase/activity/SplashActivity.kt
@@ -5,33 +5,64 @@ import android.animation.ValueAnimator.RESTART
 import android.animation.ValueAnimator.REVERSE
 import android.content.Intent
 import android.databinding.ViewDataBinding
+import android.os.Build
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageView
 import cn.bingoogolapple.bgabanner.BGALocalImageSize
 import com.lz.framecase.R
-import com.lz.framecase.R.id.*
+
+
 import com.lz.framecase.base.BaseActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 import kotlinx.android.synthetic.main.activity_splash.*
+import org.reactivestreams.Subscriber
+import java.util.concurrent.TimeUnit
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+import android.os.Build.VERSION.SDK_INT
+import android.view.View
+import com.lz.framecase.R.id.bgbanner
+import com.lz.framecase.R.id.svgimage
+import com.lz.utilslib.interceptor.utils.LzStatueBarUtils
 
 
 class SplashActivity : BaseActivity<ViewDataBinding>() {
+
+
+    var subscribe: Disposable? = null
     override fun getLayout(): Int {
         return R.layout.activity_splash
     }
 
     override fun initViewData() {
-        val ofFloat = ObjectAnimator.ofFloat(root_layout, "scaleY",  1f, 1.2f)
+        val ofFloat = ObjectAnimator.ofFloat(bgbanner, "scaleY", 1f, 1.2f)
         ofFloat.setDuration(5000)
         ofFloat.repeatMode = REVERSE
         ofFloat.repeatCount = 1000
         ofFloat.start()
-        val ofFloat2 = ObjectAnimator.ofFloat(root_layout, "scaleX",  1f, 1.2f)
+        val ofFloat2 = ObjectAnimator.ofFloat(bgbanner, "scaleX", 1f, 1.2f)
         ofFloat2.setDuration(5000)
         ofFloat2.repeatMode = REVERSE
         ofFloat2.repeatCount = 1000
         ofFloat2.start()
 
-
-
+        window
+        subscribe = Observable
+                .timer(5000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(Consumer {
+                    if (!isDestroyed) {
+                        startActivity(Intent(mActivity, MainActivity::class.java))
+                        svgimage.stopAnimation()
+                        finish()
+                    }
+                })
         val localImageSize = BGALocalImageSize(720, 1280, 320f, 640f)
         bgbanner.setData(localImageSize, ImageView.ScaleType.CENTER_CROP,
                 R.mipmap.main_bg)
@@ -41,5 +72,14 @@ class SplashActivity : BaseActivity<ViewDataBinding>() {
             finish()
         })
 
+        LzStatueBarUtils.setSystemUIVisible(this, false)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (subscribe != null) {
+            subscribe!!.dispose()
+            subscribe = null
+        }
     }
 }
---
+++ utilslib/src/main/java/com/lz/utilslib/interceptor/utils/LzStatueBarUtils.java
@@ -0,0 +1,78 @@
+package com.lz.utilslib.interceptor.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.view.View;
+
+import com.lz.fram.app.FrameApplication;
+
+/**
+ * -----------作者----------日期----------变更内容-----
+ * -          刘泽      2018-09-13       创建class
+ */
+public class LzStatueBarUtils {
+    public static void hideNavigationBar(Activity context) {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = context.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = context.getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+    public static void showNavigationBar(Activity context) {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = context.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.VISIBLE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = context.getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+
+    }
+
+    //状态栏隐藏侠士
+    public static void setStatusBarVisible(Activity context, boolean show) {
+        if (show) {
+            int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            uiFlags |= 0x00001000;
+            context.getWindow().getDecorView().setSystemUiVisibility(uiFlags);
+        } else {
+            int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            uiFlags |= 0x00001000;
+            context.getWindow().getDecorView().setSystemUiVisibility(uiFlags);
+        }
+    }
+
+    /**
+     * 导航栏状态栏显示隐藏
+     *
+     * @param context
+     * @param show
+     */
+    public static void setSystemUIVisible(Activity context, boolean show) {
+        if (show) {
+            int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            uiFlags |= 0x00001000;
+            context.getWindow().getDecorView().setSystemUiVisibility(uiFlags);
+        } else {
+            int uiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            uiFlags |= 0x00001000;
+            context.getWindow().getDecorView().setSystemUiVisibility(uiFlags);
+        }
+    }
+}
